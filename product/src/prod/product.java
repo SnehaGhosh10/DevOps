7d70c55309c18500be936dcd69b87711e8bcf914
Sneha Ghosh <ghoshsneha203@gmail.com>
2026-03-12 11:51:52 +0530

    uploading files

---
+++ product/src/prod/product.java
@@ -0,0 +1,12 @@
+package prod;
+
+public class product {
+    static void main() {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        System.out.println(a * b * c * d);
+
+    }
+}
